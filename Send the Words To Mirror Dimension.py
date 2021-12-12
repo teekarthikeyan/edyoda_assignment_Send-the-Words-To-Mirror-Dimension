533441e2e10d09f5e19129d95287a258187db664
teekarthikeyan <96018175+teekarthikeyan@users.noreply.github.com>
2021-12-12 23:10:03 +0530

    Add files via upload

---
+++ Send the Words To Mirror Dimension.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+word = input("type the word to reverse? ")
+
+for i in range (len(word) -1, -1, -1):
+    print(word[i], end="")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
